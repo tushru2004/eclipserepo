ce352218270f15ef0e38d9c87f5dfc6ae6e5dd7c
Tushar Timande <tdt7348@rit.edu>
2016-02-18 16:16:51 -0500

    First Commit

---
+++ algos/src/algos/QuickSort.java
@@ -0,0 +1,32 @@
+package algos;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public int[] Quick(int [] input,int start,int end) {
+		// TODO Auto-generated method stub
+       if(input.length==0 || input.length==1) return input;
+       if(start>end) return input;
+       
+       int pos= Partition (input, start, end);
+       
+       Quick(input,0,pos-1);
+       
+       Quick(input,pos+1, input.length-1);
+       
+       return input;
+	}
+	
+	public int Partition(int [] A, int start, int end)
+	{
+		int pos= A[end];
+		for(int i = start; i<end;i++)
+		{
+			
+		}
+		return 1;
+	}
+}
